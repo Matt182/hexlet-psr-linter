57a895c4abcd19ace8f95167b23abdbc3c3d72dd
matt182 <mattspring0@gmail.com>
2016-07-31 13:15:57 +0300

    function check test added

--- src/checkers.php
+++ src/checkers.php
@@ -5,7 +5,7 @@ namespace hexletPsrLinter;
 function checkFunctionName($name)
 {
     $regex = "^[a-z]+([A-Z]?[a-z]+)+$";
-    $result = preg_match_all("/$regex/" ,$name);
+    $result = preg_match_all("/$regex/", $name);
     if ($result == 0) {
         return false;
     } else {
