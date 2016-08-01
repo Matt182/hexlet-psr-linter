bcf85b7b8ed804cdb9db74d8540e742406452ace
matt182 <mattspring0@gmail.com>
2016-08-01 21:58:15 +0300

    pull request test

--- src/checkers.php
+++ src/checkers.php
@@ -6,9 +6,6 @@ function checkFunctionName($name)
 {
     $regex = "^[a-z]+([A-Z]?[a-z]+)+$";
     $result = preg_match_all("/$regex/", $name);
-    if ($result == 0) {
-        return false;
-    } else {
-        return true;
-    }
+    if ($result == 0) return false;
+    return true;
 }
