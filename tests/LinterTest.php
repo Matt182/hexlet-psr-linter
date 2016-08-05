970a740e24932429e500e20cdb11584e57eb049e
matt182 <mattspring0@gmail.com>
2016-08-06 02:32:06 +0300

    tests edited vfs added

--- tests/LinterTest.php
+++ tests/LinterTest.php
@@ -3,15 +3,37 @@
 namespace HexletPsrLinter;
 
 use HexletPsrLinter\Linter;
+use org\bovigo\vfs\vfsStream;
 
 class LinterTest extends \PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        $this->root = vfsStream::setup('root');
+        $structure = array(
+                'innerFixture' => array(
+                    'innerTest.php' => '<?php
+                    function InnerWrong() {} ?>'),
+                'test.php' => '<?php
+                    function re_qwe() {}
+                    function ok() {}
+                    class Test {
+                        function Wrong() {}
+                        function ok() {}
+                    }?>',
+            );
+            vfsStream::create($structure, $this->root);
+    }
+
     public function testRunner()
     {
+        $rootPath = vfsStream::url($this->root->getName());
+
+
         $path = __DIR__.'/fixtures';
-        $result = run($path);
+        $result = run($rootPath);
         $this->assertEquals(
-            [[2,'function','re_qwe'],[14,'method','Wrong'],[2, 'function', 'InnerWrong']],
+            [[2, 'function', 'InnerWrong'], [2,'function','re_qwe'], [5,'method','Wrong']],
             $result
         );
     }
