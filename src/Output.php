573925d11dfebe43dbdcdb4d679c8c2fcecfc44e
matt182 <mattspring0@gmail.com>
2016-08-08 23:31:22 +0300

    output edited

--- src/Output.php
+++ src/Output.php
@@ -5,9 +5,8 @@ function printResult($messages)
 {
     if (empty($messages)) {
         echo "Congratulation! everything is ok.\n";
-    } else {
-        foreach ($messages as $message) {
-            echo "Warning on line:{$message[0]} {$message[1]} name must be in camelCaps() ({$message[2]})\n";
-        }
+    }
+    foreach ($messages as $message) {
+        echo "Warning on line:{$message[0]} {$message[1]} name must be in camelCaps() ({$message[2]})\n";
     }
 }
